dcc90656675750775454e53efdcdf87c06203b90
Hercules <skaldi.ltd@gmail.com>
2020-07-24 13:54:17 +0300

    change config

--- vue.config.js
+++ vue.config.js
@@ -35,11 +35,9 @@ module.exports = {
 
   // output built static files to Laravel's public dir.
   // note the "build" script in package.json needs to be modified as well.
-  outputDir: '../public',
+  outputDir: './public',
 
   // modify the location of the generated HTML file.
   // make sure to do this only in production.
-  indexPath: process.env.NODE_ENV === 'production'
-    ? '../resources/views/index.blade.php'
-    : 'index.html',
+  indexPath: '../index.html',
 };
