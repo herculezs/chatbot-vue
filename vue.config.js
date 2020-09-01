c9caea648e10ae7b81d7f9aec11038fe448e3910
Hercules <skaldi.ltd@gmail.com>
2020-09-01 16:31:53 +0300

    change output dir

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
 
   // output built static files to Laravel's public dir.
   // note the "build" script in package.json needs to be modified as well.
-  outputDir: '../',
+  outputDir: process.env.OUTPUT_DIR,
 
   // modify the location of the generated HTML file.
   // make sure to do this only in production.
