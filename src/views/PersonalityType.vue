2613d8abf53686817a998c509d359b1f64841875
Hercules <skaldi.ltd@gmail.com>
2020-09-09 13:33:43 +0300

    change text

--- src/views/PersonalityType.vue
+++ src/views/PersonalityType.vue
@@ -2,7 +2,7 @@
   <div class="personality-type">
     <Content>
       <h1 class="h5">
-        Which type best represents how you appear at work?
+        Which personality type do you think best represents how you see yourself ?
       </h1>
       <PersonalityCarousel :slides="slides" @change="changeSlide" />
       <button
