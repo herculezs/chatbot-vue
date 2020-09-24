583677f1840531b3f0d8546c1ef82d3c878361b4
Hercules <skaldi.ltd@gmail.com>
2020-09-24 11:41:36 +0300

    change button personality-type

--- src/components/PersonalityCarousel/PersonalityCarousel.vue
+++ src/components/PersonalityCarousel/PersonalityCarousel.vue
@@ -18,11 +18,40 @@
       </div>
     </VueSlickCarousel>
     <div class="personality-type-carousel__controls">
-      <button class="personality-type-carousel__button" @click="showPrev">‹</button>
+      <button class="personality-type-carousel__button" @click="showPrev">
+        <svg xmlns="http://www.w3.org/2000/svg"
+             xmlns:xlink="http://www.w3.org/1999/xlink"
+             version="1.1"
+             x="0px" y="0px"
+             viewBox="0 0 256 256"
+             style="enable-background:new 0 0 256 256;" xml:space="preserve"
+        >
+          <g>
+            <g>
+              <polygon
+                points="207.093,30.187 176.907,0 48.907,128 176.907,
+                256 207.093,225.813 109.28,128"/>
+            </g>
+          </g>
+        </svg>
+      </button>
       <div class="personality-type-carousel__controls-text">
         Swipe left or right to select personality
       </div>
-      <button class="personality-type-carousel__button" @click="showNext">›</button>
+      <button class="personality-type-carousel__button" @click="showNext">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 256 256"
+          style="enable-background:new 0 0 256 256;" xml:space="preserve"
+        >
+          <g>
+            <g>
+              <polygon points="79.093,0 48.907,30.187 146.72,
+              128 48.907,225.813 79.093,256 207.093,128"/>
+            </g>
+          </g>
+        </svg>
+      </button>
     </div>
   </div>
 </template>
@@ -99,7 +128,7 @@ export default {
   .personality-type-carousel__controls-text{
     color: $txtColor2;
     font-family: $defaultFont;
-    font-size: 14px;
+    font-size: 16px;
     letter-spacing: 0;
     line-height: 16px;
     text-align: center;
@@ -111,6 +140,11 @@ export default {
     color: $txtColor2;
     font-size: 32px;
     line-height: 8px;
+    width: 24px;
+    height: 24px;
+    svg{
+      fill: $txtColor2;
+    }
     &:hover, &:focus{
       outline: none;
     }
