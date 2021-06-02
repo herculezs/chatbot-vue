cf0d4347d6e9656e83fb10778ec24b78ce632333
Hercules <skaldi.ltd@gmail.com>
2021-06-02 12:59:01 +0300

    new env corp

---
+++ src/profiles/corp/index.js
@@ -0,0 +1,56 @@
+export default {
+  report: {
+    textForReport: {
+      title: 'Crowdsource your Social Persona',
+      paragraph1: 'Your report so far is what <b>you think your personality is</b>. This is called your Anima.'
+        + ' The "you" that others experience is called your Persona. We'
+        + ' can find out what your Social Persona is by asking people you'
+        + ' know socially to answer questions about you.',
+      paragraph2: 'Share the link with your friends and family - they can answer a'
+        + ' similar questionnaire about you and all answers remain'
+        + ' totally anonymous. We do this so everyone can be more open and'
+        + ' honest than they might be if they were asked directly.',
+      paragraph3: 'You need to get at least <b>4 responses</b> for us to show the'
+        + ' anonymised results. We will continue to update you as we receive more responses.',
+    },
+  },
+  charts: {
+    pointColor: '#b990da',
+    backGroundColorLabel: '#ffffff',
+    chosePointColor: '#4d1187',
+  },
+  starRatingReport: {
+    starActiveColor: '#7811C9',
+    starBorderColor: '#7811C9',
+  },
+  personalityType: {
+    title: 'Here are the different personality types:'
+      + ' hover over each dot to see a brief summary then click'
+      + ' "Show Results" when you are ready',
+  },
+  inputCopy: {
+    copyText: 'Hi, {{firstName}} {{secondName}} would like you to click the link below and fill in the survey. {{firstName}} is trying to find out what their personality is like. We find out what their personality is by asking people they know.\n\n'
+      + 'Your answers are totally anonymous - you only see what you have answered and those answers are aggregated up so that no one can tell what you have said.\n\n'
+      + 'We made it anonymous as we hope you can be more open and honest than you might be if you were asked directly.\n\n'
+      + 'Once we get at a minimum of 3 other responses, we will show you the anonymized results. You can go through the process yourself once you have answered {{firstName}}\'s questions.\n\n'
+      + 'Here is the link:\n\n',
+    copyText2: '',
+  },
+  onboarding: {
+    defaultStatePhone: 'GB',
+    placeholderPhone: '12345-678901',
+    emailPlaceHolder: 'Email',
+    tooltipEmail: 'Your email, so we can let you know what others are finished talking about you',
+    buttonOnFirstPage: 'Get Started',
+    step1: 'Step 1: You will be asked 10 short questions about you and then you will receive your first report ...',
+    step2: 'Step 2: Ask your contacts to answer 10 short questions about you to receive your comparative report.',
+  },
+  registerPage: {
+    titleDetailText: 'And don\'t worry, any answers you give about others will be completely anonymous to them',
+  },
+  testdashboard: {
+    defaultCodePhone: '+44',
+    placeholderPhoneDashboard: '+4412345-678901',
+    defaultStatePhone: 'GB',
+  },
+};
