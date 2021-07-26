a7fabf92a010990748fae50ee17f2737b3fbe7b8
Hercules <skaldi.ltd@gmail.com>
2021-07-26 18:01:26 +0300

    changed text

--- src/profiles/test/index.js
+++ src/profiles/test/index.js
@@ -5,7 +5,7 @@ export default {
       paragraph1: 'Your report so far is what <b>you think your personality is</b>. This is called your Anima.'
         + ' The "you" that others experience is called your Persona. We'
         + ' can find out what your Social Persona is by asking people you'
-        + ' know socially to answer questions about you.'
+        + ' know socially to answer questions about you. '
         + 'Below, you can select Friends, Family or choose another social group who\'s an opinion you want.',
       paragraph2: 'Share the the specific link with the specific group so that they can'
         + ' answer a similar questionnaire about you and all answers remain totally anonymous'
@@ -13,8 +13,7 @@ export default {
         + 'they were asked directly.',
       paragraph3: 'You need to get at least <b>4 responses</b> for us to show the'
         + ' anonymised results. We will continue to update you as we receive more responses.',
-      paragraph4: 'Use the copy button {copy-icon}'
-        + ' anonymised results. We will continue to update you as we receive more responses.',
+      paragraph4: 'Use the copy button anonymised results. We will continue to update you as we receive more responses.',
     },
   },
   charts: {
